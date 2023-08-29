b74b54386e0a9bf46390c83f397995bd9b840ecf
Supherk <Cryphts@gmail.com>
2023-08-30 05:43:19 +0800

    init

---
+++ docs/assets/pages-setting-setting.7c68017a.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,w as r,i as s,l as t}from"./index-8c7aba04.js";import{_ as o}from"./plugin-vueexport-helper.1b428a4d.js";const n=o({data:()=>({})},[["render",function(o,n,i,l,u,p){const d=s;return a(),e(d,null,{default:r((()=>[t(" 设置中心 ")])),_:1})}]]);export{n as default};
