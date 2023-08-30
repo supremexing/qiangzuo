d05fa7f8664ec4195c305df24826a67a0894f976
Supherk <Cryphts@gmail.com>
2023-08-30 16:37:15 +0800

    0.8.26.1100

---
+++ docs/assets/pages-setting-setting.1e1a3967.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,w as r,i as s,l as t}from"./index-a42ec16d.js";import{_ as o}from"./plugin-vueexport-helper.1b428a4d.js";const n=o({data:()=>({})},[["render",function(o,n,i,l,u,p){const d=s;return a(),e(d,null,{default:r((()=>[t(" 设置中心 ")])),_:1})}]]);export{n as default};
